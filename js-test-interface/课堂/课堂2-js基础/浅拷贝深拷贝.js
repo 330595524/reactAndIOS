95bc80ec47c07289c757139861e77ee951410201
徐历敏 <liminxu2@sf-express.com>
2020-02-18 09:12:28 +0800

    commit fixed

---
+++ js-test-interface/课堂/课堂2-js基础/浅拷贝深拷贝.js
@@ -0,0 +1,14 @@
+function deploy(obj) {
+    
+}
+
+var objstart = {
+    name:'xiaoming',
+    age:12,
+    say:function (params) {
+        
+    },
+    work:undefined
+}
+var obj2 = objstart;
+obj2.name = 'hahhaha';
