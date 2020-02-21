53166efbfd6291f043f476a0c48239a3d8ad2331
徐历敏 <liminxu2@sf-express.com>
2020-02-21 19:58:35 +0800

    样式修改

--- js-test-interface/课堂/课堂2-js基础/浅拷贝深拷贝.js
+++ js-test-interface/课堂/课堂2-js基础/浅拷贝深拷贝.js
@@ -1,5 +1,19 @@
 function deploy(obj) {
+    var result = Array.isArray(obj)?[]:{};
+    if(obj && typeof obj === 'object'){
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                
+                if(obj[key]&& typeof obj[key] ==='object'){
+                    result[key] = deploy(obj[key]);
+                }else{
+                    result[key] = obj[key];
+                }
+            }
+        }
+    }
     
+   return result;
 }
 
 var objstart = {
@@ -8,7 +22,18 @@ var objstart = {
     say:function (params) {
         
     },
-    work:undefined
+    work:undefined,
+    home:{
+        child:123
+    }
 }
-var obj2 = objstart;
+var obj2 = deploy(objstart);
 obj2.name = 'hahhaha';
+obj2.home.child = 'hahhaha2222';
+console.log(objstart);
+console.log(obj2);
+
+let obj = {};
+obj.name = '班班开心';
+
+console.log(obj.hasOwnProperty('name'));// 非继承
