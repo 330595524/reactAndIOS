c0e5a014b34bf2fde0d126c6e1c5cb6e7235161e
徐历敏 <330595524@qq.com>
2020-05-21 14:49:47 +0800

    首页头，底部完结

---
+++ jz/homePage/js/common.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: xulimin
+ * @Date: 2020-05-21
+ * @Last Modified by: xulimin
+ * @Last Modified time: 2020-05-21
+ */
+
+  // console.log('执行了')
+  // $(".home_header a").click(function() {
+  //   console.log('执行了')
+  //   var _index = $(this).index();
+  //   // $(".model_cont_box .model_div").eq(_index).addClass('show').siblings().removeClass("show");
+  //   //改变选中时候的选项框的样式，移除其他几个选项的样式
+  //   $(this).addClass("active").siblings().removeClass("active");
+  // });
