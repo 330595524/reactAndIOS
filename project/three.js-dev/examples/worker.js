46b252391a0ee70d1307656c842250bc993c61a0
徐历敏 <330595524@qq.com>
2020-05-26 09:37:45 +0800

    “首页完成”

--- project/three.js-dev/examples/worker.js
+++ project/three.js-dev/examples/worker.js
@@ -4,3 +4,13 @@
  * @Last Modified by: xulimin
  * @Last Modified time: 2020-05-25
  */
+//使用onmessage回调函数来获取从主线程传递来的数据
+onmessage = function(e) {
+  console.log("进来了哦")
+  console.log(navigator,location)
+  let de = e.data.map(item=>item*5)
+  // var workerResult = 'Result: ' + de; //将获取到的值乘以5再返回
+  postMessage(de); //调用postMessage将数据传递给主线程
+  this.close();
+};
+
